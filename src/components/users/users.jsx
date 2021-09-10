e94b6ec0e794fdde41191c29c93489ec1a5746d7
Anna Zubareva <annazubareva14@gmail.com>
2021-09-10 15:29:25 +0300

    add pagination in users page

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -7,37 +7,60 @@ import userBackground from "../../assets/images/userBackground.jpeg"
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
     }
 
     render() {
-        return <div className={s.friendList}>
-            {
-                this.props.users.map(u => <div key={u.id} className={s.friendsCardWrapper}>
-                    <div className={s.friendCard}>
-                        <img src={u.background != null ? u.background : userBackground} alt="profile-cover" className={s.background} />
-                        <div className={s.cardInfo}>
-                            <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="user" className={s.photo} />
-                            <div className={s.info}>
-                                <div className={s.userInfo}>
-                                    <h5><a href="#" className={s.profileLink}> {u.name} </a></h5>
-                                    <p> {u.status} </p>
-                                    <p> {"u.location.city"}, {"u.location.country"} </p>
-                                </div>
-                                <div className={s.btnWrapper}>
-                                    {u.followed
-                                        ? <button onClick={() => { this.props.unfollow(u.id) }} className={s.btnUnfollow} >unfollow</button>
-                                        : <button onClick={() => { this.props.follow(u.id) }} className={s.btnFollow} >follow</button>
-                                    }
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+        return <div className={s.pageWrapper}>
+            <div className={s.pagibation}>
+                { pages.map(p => {
+                    return <span className={this.props.currentPage === p && s.selectedPage}
+                            onClick={(e) => {this.onPageChanged(p);}}> {p} </span>
+                }) }
+            </div>
+            <div className={s.friendList}>
+                {
+                    this.props.users.map(u => <div key={u.id} className={s.friendsCardWrapper}>
+                        <div className={s.friendCard}>
+                            <img src={u.background != null ? u.background : userBackground} alt="profile-cover" className={s.background} />
+                            <div className={s.cardInfo}>
+                                <img src={u.photos.small != null ? u.photos.small : userPhoto} alt="user" className={s.photo} />
+                                <div className={s.info}>
+                                    <div className={s.userInfo}>
+                                        <h5><a href="#" className={s.profileLink}> {u.name} </a></h5>
+                                        <p> {u.status} </p>
+                                        <p> {"u.location.city"}, {"u.location.country"} </p>
+                                    </div>
+                                    <div className={s.btnWrapper}>
+                                        {u.followed
+                                            ? <button onClick={() => { this.props.unfollow(u.id) }} className={s.btnUnfollow} >unfollow</button>
+                                            : <button onClick={() => { this.props.follow(u.id) }} className={s.btnFollow} >follow</button>
+                                        }
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>)
-            }
+                    </div>)
+                }
+            </div>
+
         </div>
     }
 }
--- src/components/users/usersContainer.jsx
+++ src/components/users/usersContainer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Users from "./users";
 import { connect } from "react-redux";
-import { followAC, setUsersAC, unfollowAC } from "../../redux/usersPage-reducer";
+import { followAC, setUsersAC, unfollowAC, setCurrentPageAC, setTotalUsersCountAC } from "../../redux/usersPage-reducer";
 
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSize: state.usersPage.pageSize,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 };
 
@@ -20,6 +23,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUsersAC(users))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 };
